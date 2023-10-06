4397862b3f8c789265f8a543172e3551e82a3470
Sarah <Sarah@HP-SARAHI>
2023-10-05 22:03:28 -0600

    Agregar archivos de proyecto.

---
+++ Graph.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+    internal class Graph
+    {
+        private int V; // Number of vertices
+        private List<int>[] adjacencyList; // Adjacency list to represent the graph
+
+        public Graph(int v)
+        {
+            V = v;
+            adjacencyList = new List<int>[v];
+            for (int i = 0; i < v; ++i)
+            {
+                adjacencyList[i] = new List<int>();
+            }
+        }
+
+        public void AddEdge(int v, int w)
+        {
+            adjacencyList[v].Add(w);
+            adjacencyList[w].Add(v);
+        }
+
+        public void DisplayGraph()
+        {
+            for (int i = 0; i < V; ++i)
+            {
+                Console.Write("Vertex " + i + ": ");
+                foreach (var node in adjacencyList[i])
+                {
+                    Console.Write(node + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfVertices = 5;
+            Graph graph = new Graph(numberOfVertices);
+
+            graph.AddEdge(0, 1);
+            graph.AddEdge(0, 4);
+            graph.AddEdge(1, 2);
+            graph.AddEdge(1, 3);
+            graph.AddEdge(1, 4);
+            graph.AddEdge(2, 3);
+            graph.AddEdge(3, 4);
+
+            Console.WriteLine("Graph Representation:");
+            graph.DisplayGraph();
+            Console.ReadKey();
+        }
+    }
+}
